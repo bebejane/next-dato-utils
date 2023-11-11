cd99a1e95e8f7a352eee00081f0e5805a3392352
Bebe Jane <bjornthief@gmail.com>
2023-11-12 00:55:36 +0100

    updates

--- dist/api/index.d.ts
+++ dist/api/index.d.ts
@@ -1,1 +1,1 @@
-export { default as apiQuery, generateIdTags } from './api-query';
+export { default as apiQuery } from './api-query';
--- dist/api/index.js
+++ dist/api/index.js
@@ -3,8 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateIdTags = exports.apiQuery = void 0;
+exports.apiQuery = void 0;
 var api_query_1 = require("./api-query");
 Object.defineProperty(exports, "apiQuery", { enumerable: true, get: function () { return __importDefault(api_query_1).default; } });
-Object.defineProperty(exports, "generateIdTags", { enumerable: true, get: function () { return api_query_1.generateIdTags; } });
 //# sourceMappingURL=index.js.map
