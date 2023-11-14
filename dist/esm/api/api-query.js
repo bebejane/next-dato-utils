ba3619134ddce19ef214fb3b88eeb407caab16e0
Bebe Jane <bjornthief@gmail.com>
2023-11-14 22:16:58 +0100

    updates

--- dist/esm/api/api-query.js
+++ dist/esm/api/api-query.js
@@ -32,9 +32,9 @@ export default async function apiQuery(query, options) {
         queryId
     };
     const tags = opt.generateTags ? generateIdTags(await dedupedFetch(dedupeOptions), opt.tags, queryId) : opt.tags;
-    const res = includeDrafts ? await dedupedFetch({ ...dedupeOptions, url: 'https://graphql-listen.datocms.com/preview' }) : {};
+    const res = includeDrafts ? await dedupedFetch({ ...dedupeOptions, tags, url: 'https://graphql-listen.datocms.com/preview' }) : {};
     const { data } = await dedupedFetch({ ...dedupeOptions, tags });
-    console.log(queryId, opt, tags);
+    console.log(queryId, { ...opt, tags });
     return { ...data, draftUrl: res.url ?? null };
 }
 const dedupedFetch = cache(async (options) => {
--- src/api/api-query.ts
+++ src/api/api-query.ts
@@ -59,13 +59,11 @@ export default async function apiQuery<T, V>(query: DocumentNode, options?: ApiQ
     queryId
   }
 
-
-
   const tags = opt.generateTags ? generateIdTags(await dedupedFetch(dedupeOptions), opt.tags, queryId) : opt.tags
-  const res = includeDrafts ? await dedupedFetch({ ...dedupeOptions, url: 'https://graphql-listen.datocms.com/preview' }) : {}
+  const res = includeDrafts ? await dedupedFetch({ ...dedupeOptions, tags, url: 'https://graphql-listen.datocms.com/preview' }) : {}
   const { data } = await dedupedFetch({ ...dedupeOptions, tags });
 
-  console.log(queryId, opt, tags)
+  console.log(queryId, { ...opt, tags })
 
   return { ...data, draftUrl: res.url ?? null }
 }
