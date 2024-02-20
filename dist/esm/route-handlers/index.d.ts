ae5f7c9df319aaf181d492a284adddfeef98c5e9
Bebe Jane <bjornthief@gmail.com>
2024-02-20 13:19:23 +0100

    ja

---
+++ dist/esm/route-handlers/index.d.ts
@@ -0,0 +1,9 @@
+export { default as backup } from './backup.js';
+export { default as basicAuth } from './basic-auth.js';
+export { default as cors } from './cors.js';
+export { default as middleware } from './middleware.js';
+export { default as revalidate } from './revalidate.js';
+export { default as test } from './test.js';
+export { default as vercelCronAuth } from './vercel-cron-auth.js';
+export { default as webPreviews } from './web-previews.js';
+export { default as draft } from './draft.js';
