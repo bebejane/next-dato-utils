dd7394ab267ed6956378a7c4aa1ec38221ca0cdf
Bebe Jane <bjornthief@gmail.com>
2025-01-24 19:32:54 +0100

    updates

--- cjs/api/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiQuery = void 0;
-var api_query_js_1 = require("./api-query.js");
-Object.defineProperty(exports, "apiQuery", { enumerable: true, get: function () { return __importDefault(api_query_js_1).default; } });
-//# sourceMappingURL=index.js.map
