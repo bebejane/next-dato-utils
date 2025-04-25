cf45fa7caa456efbe17eb7d3f683b810782cb4f0
Bebe Jane <bjornthief@gmail.com>
2025-04-25 10:39:12 +0200

    updates

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from 'next';
 import { backup, revalidate, test, webPreviews, draft } from '../route-handlers';
 import { cosmiconfig } from 'cosmiconfig';
-import { TypeScriptLoaderSync, TypeScriptLoader } from 'cosmiconfig-typescript-loader';
+import { cache } from 'react';
+
+import { TypeScriptLoader } from 'cosmiconfig-typescript-loader';
 
 
 export type DatoCmsConfig = {
@@ -25,7 +27,8 @@ export type DatoCmsConfig = {
   sitemap?: () => Promise<MetadataRoute.Sitemap>
 }
 
-export const getDatoCmsConfig = async (): Promise<DatoCmsConfig> => {
+export const getDatoCmsConfig = cache(async (): Promise<DatoCmsConfig> => {
+
   const explorer = cosmiconfig('datocms', {
     searchPlaces: ['datocms.config.ts', 'datocms.config.json'], // Explicitly search for the TS file 
     loaders: { '.ts': TypeScriptLoader() },
@@ -38,7 +41,8 @@ export const getDatoCmsConfig = async (): Promise<DatoCmsConfig> => {
   console.log(res.config)
   // The config object is nested under the 'config' property
   return res.config as DatoCmsConfig;
-}
+})
+
 
 export const getRoute = async (record: any, locale?: string): Promise<string[] | null> => {
   const config = await getDatoCmsConfig()
