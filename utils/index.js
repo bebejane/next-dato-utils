80021544b3594930620223d922dbe4a08f3890c6
Bebe Jane <bjornthief@gmail.com>
2025-09-08 09:14:02 +0200

    updates

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 export { default as markdownTruncate } from './markdown-truncate.js';
 export { default as iterateObject } from './iterate-object.js';
 export { sendPostmarkEmail } from './postmark.js';
-export * from './utils';
+export * from './utils.js';
 //# sourceMappingURL=index.js.map
