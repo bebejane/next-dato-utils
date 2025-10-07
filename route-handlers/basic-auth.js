310f415b01a9efbdd575934b7c7e8f902744e081
Bebe Jane <bjornthief@gmail.com>
2025-10-07 15:06:32 +0200

    updates

--- route-handlers/basic-auth.js
+++ route-handlers/basic-auth.js
@@ -1,3 +1,5 @@
+const u = process.env.BASIC_AUTH_USER;
+const p = process.env.BASIC_AUTH_PASSWORD;
 export default async function basicAuth(req, callback, options) {
     if (req.method === 'OPTIONS')
         return new Response('OK', { status: 200 });
@@ -9,10 +11,10 @@ export default async function basicAuth(req, callback, options) {
     const auth = basicAuth.split(' ')[1];
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':');
     const username = options?.username || process.env.BASIC_AUTH_USER;
-    const password = options?.password || process.env.BASIC_AUTH_USER;
+    const password = options?.password || process.env.BASIC_AUTH_PASSWORD;
     const isAuthorized = user === username && pwd === password;
     if (!isAuthorized) {
-        console.log(username, password, process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_USER, user, pwd);
+        console.log(username, password, process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_PASSWORD, user, pwd);
         return new Response('Access denied. Wrong password or username.', { status: 401 });
     }
     if (callback)
--- src/route-handlers/basic-auth.ts
+++ src/route-handlers/basic-auth.ts
@@ -1,3 +1,6 @@
+const u = process.env.BASIC_AUTH_USER;
+const p = process.env.BASIC_AUTH_PASSWORD;
+
 export default async function basicAuth(
 	req: Request,
 	callback?: (req: Request) => Promise<Response>,
@@ -15,11 +18,11 @@ export default async function basicAuth(
 	const auth = basicAuth.split(' ')[1];
 	const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':');
 	const username = options?.username || process.env.BASIC_AUTH_USER;
-	const password = options?.password || process.env.BASIC_AUTH_USER;
+	const password = options?.password || process.env.BASIC_AUTH_PASSWORD;
 	const isAuthorized = user === username && pwd === password;
 
 	if (!isAuthorized) {
-		console.log(username, password, process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_USER, user, pwd);
+		console.log(username, password, process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_PASSWORD, user, pwd);
 		return new Response('Access denied. Wrong password or username.', { status: 401 });
 	}
 
