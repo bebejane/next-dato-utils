bdaa48e87a65378dcb7915aba975b3e0a144b566
Bebe Jane <bjornthief@gmail.com>
2026-03-21 13:14:48 +0100

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,9 @@
 export { type DatoCmsConfig } from './config.js';
-export { getItemReferenceRoutes, getUploadReferenceRoutes, getItemWithLinked, loadConfig } from './utils.js';
+export {
+	getRoute,
+	getItemReferenceRoutes,
+	getUploadReferenceRoutes,
+	getItemWithLinked,
+	loadConfig,
+} from './utils.js';
 export { default as withDatoCms } from './with-datocms.js';
